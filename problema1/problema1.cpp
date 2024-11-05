ad55c2cee8513e52ec665e23eeb85ac2304c08d7
AlejandroColl006 <acolllen@myuax.com>
2024-11-05 10:51:59 +0100

    Problema 1 realizado

---
+++ problema1/problema1.cpp
@@ -0,0 +1,16 @@
+#include "problema1.h"
+#include <iostream>
+int main() {
+    int numero;
+    std::cout << "Ingrese un numero: ";
+    std::cin >> numero;
+
+    if (numero < 0) {
+        std::cout << "El numero ingresado negativo";
+    } else if (numero == 0) {
+        std::cout << "El numero ingresado cero";
+    } else if (numero > 0) {
+        std::cout << "El numero ingresado positivo";
+    }
+    return 0;
+}
